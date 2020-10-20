bffcdb3a0017a429190da542817a9b275d1655cd
Matt <mjruddy94@gmail.com>
2020-10-20 10:28:46 -0400

    test

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,8 +2,8 @@ export const blogUrl = (title: string) =>  `https://fundrise.cdn.prismic.io/api/
 export const blogUrlSlug = (title: string) =>  `https://fundrise.cdn.prismic.io/api/v2/documents/search?ref=X4nauBEAAB4AbDwr&q=[[at(my.blog.uid,%20%22${title}%22)]]&fetchLinks=categories.category_name,blog.title,blog.abstract,blog.cover_image`
 
 export const Branding = {
-    defaultDesciption: "Tross Capital",
-    defaultLogo: "logo.png",
+    defaultDesciption: "this is a test bro",
+    defaultLogo: "/logo.png",
     defaultTitle: "Tross Capital",
     defaultUrl: ""
 }
