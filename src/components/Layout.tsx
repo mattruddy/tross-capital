3381fb0e13b37daacb3e9759ceeecd13f408ccdc
Matt <mjruddy94@gmail.com>
2020-10-20 20:08:01 -0400

    clean up

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -23,7 +23,6 @@ const Layout = ({ children, title = 'Tross Capital', metaTitle, description, ima
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
-      <link rel="manifest" href="/manifest.json" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
